e1f7edd283d9645ca7719ca5c5e128fbb3c8aa1f
Nitin Grover <grovernitin48@gmail.com>
2023-02-15 15:29:06 +0530

    update palindrome

--- src/Palindrome.js
+++ src/Palindrome.js
@@ -1,3 +1,19 @@
+// program to check if the string is palindrome or not
+function checkPalindrome(string) {
+    // find the length of a string
+    const len = string.length;
+
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+
 // Brute Force longest palindrome subString with its length
 // Time complexity: O(n^3). Space Complexity O(1)
 function longestPalStr(str) {
