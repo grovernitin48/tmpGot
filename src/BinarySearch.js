24b36dde678233a822959613087120970e536a6e
Nitin Grover <grovernitin48@gmail.com>
2023-03-07 13:17:02 +0530

    updated

--- src/BinarySearch.js
+++ src/BinarySearch.js
@@ -17,3 +17,55 @@ function binarySearch(arr, num) {
     return -1;
 }
 console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 11));
+
+class Node {
+        constructor(val) {
+            this.data = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+ 
+     var root= null;
+     
+    function printLevelOrder() {
+        var h = height(root);
+        var i;
+        for (i = 1; i <= h; i++)
+            printCurrentLevel(root, i);
+    }
+
+    function height(root) {
+        if (root == null)
+            return 0;
+        else {
+            var lheight = height(root.left);
+            var rheight = height(root.right);
+ 
+            if (lheight > rheight)
+                return (lheight + 1);
+            else
+                return (rheight + 1);
+        }
+    }
+ 
+    function printCurrentLevel(root , level) {
+        if (root == null)
+            return;
+        if (level == 1)
+            console.log(root.data + " ");
+        else if (level > 1) {
+            printCurrentLevel(root.left, level - 1);
+            printCurrentLevel(root.right, level - 1);
+        }
+    }
+ 
+     
+        root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+ 
+       printLevelOrder();
+ 
